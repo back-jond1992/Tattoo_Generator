94cb4b856267bcf3a53ee82c41fad923e4607b65
Jack Bond <b.jond7@outlook.com>
2021-06-15 22:47:51 +0100

    stated second modal

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,5 @@
+// Basic
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
@@ -33,4 +35,44 @@ function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
+}
+
+
+// Intermediate
+
+const openModalButtons2 = document.querySelectorAll('[data-modal-target2]')
+const closeModalButtons2 = document.querySelectorAll('[data-close-button2]')
+const overlay2 = document.getElementById('overlay2')
+
+openModalButtons2.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal2 = document.querySelector(button.dataset.modalTarget)
+        openModal2(modal2)
+    })
+})
+
+overlay2.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active2')
+    modals.forEach(modal2 => {
+        closeModal2(modal2)
+    })
+})
+
+closeModalButtons2.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal2 = button.closest('.modal2')
+        closeModal2(modal2)
+    })
+})
+
+function openModal2(modal2) {
+    if (modal2 == null) return
+    modal2.classList.add('active2')
+    overlay2.classList.add('active2')
+}
+
+function closeModal2(modal2) {
+    if (modal2 == null) return
+    modal2.classList.remove('active2')
+    overlay2.classList.remove('active2')
 }
