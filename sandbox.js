000aaed499a4547958bfd8f52a91b584b336bde6
Jack Bond <b.jond7@outlook.com>
2021-06-16 11:20:48 +0100

    added modals to each section

--- sandbox.js
+++ sandbox.js
@@ -3,6 +3,8 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+const overlay2 = document.getElementById('overlay2')
+const overlay3 = document.getElementById('overlay3')
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -12,6 +14,20 @@ openModalButtons.forEach(button => {
 })
 
 overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+overlay2.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+overlay3.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active')
     modals.forEach(modal => {
         closeModal(modal)
@@ -29,50 +45,14 @@ function openModal(modal) {
     if (modal == null) return
     modal.classList.add('active')
     overlay.classList.add('active')
+    overlay2.classList.add('active')
+    overlay3.classList.add('active')
 }
 
 function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
-}
-
-
-// Intermediate
-
-const openModalButtons2 = document.querySelectorAll('[data-modal-target2]')
-const closeModalButtons2 = document.querySelectorAll('[data-close-button2]')
-const overlay2 = document.getElementById('overlay2')
-
-openModalButtons2.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal2 = document.querySelector(button.dataset.modalTarget)
-        openModal2(modal2)
-    })
-})
-
-overlay2.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active2')
-    modals.forEach(modal2 => {
-        closeModal2(modal2)
-    })
-})
-
-closeModalButtons2.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal2 = button.closest('.modal2')
-        closeModal2(modal2)
-    })
-})
-
-function openModal2(modal2) {
-    if (modal2 == null) return
-    modal2.classList.add('active2')
-    overlay2.classList.add('active2')
-}
-
-function closeModal2(modal2) {
-    if (modal2 == null) return
-    modal2.classList.remove('active2')
-    overlay2.classList.remove('active2')
+    overlay2.classList.remove('active')
+    overlay3.classList.remove('active')
 }
