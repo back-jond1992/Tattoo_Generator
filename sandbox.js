9d75d06253f303d3228ff3591cabe63a0cb0db4c
Jack Bond <b.jond7@outlook.com>
2021-06-21 22:15:51 +0100

    addded image sliders to all levels.

--- sandbox.js
+++ sandbox.js
@@ -60,32 +60,88 @@ function closeModal(modal) {
 //modal thumbnail image JS
 //basic image slider function
 
-let basic_img = document.getElementById('basic_image');
-let b_images = [
-    Basic/dragon.jpg,
-    Basic/eagle.jpg,
-    Basic/heart.jpg,
-    Basic/mother.jpg,
-    Basic/panther.jpg,
-    Basic/rose.jpg,
-    Basic/ship.jpg,
-    Basic/skull.jpg,
-    Basic/swallow.jpg
+let basic_img = document.querySelector('#basicFlashImage');
+let basicFlashImages = [
+    'dragon.jpg',
+    'eagle.jpg',
+    'heart.jpg',
+    'mother.jpg',
+    'panther.jpg',
+    'rose.jpg',
+    'ship.jpg',
+    'skull.jpg',
+    'swallow.jpg'
 ]
+
 let i = 0;
 
 function prev() {
-    if(i <= 0) i = b_images.length;
+    if(i <= 0) i = basicFlashImages.length;
     i--;
     return setImg();
 }
 
 function next() {
-    if(i >= b_images.length-1) i=-1;
+    if(i >= basicFlashImages.length - 1) i = -1;
     i++;
     return setImg();
 }
 
 function setImg() {
-    return basic_img.setAttribute('src', 'tattoo_images/' + b_images[i])
+    return basic_img.setAttribute('src', 'tattoo_images/' + 'Basic/' + basicFlashImages[i])
+}
+
+//intermediate image slider function
+
+let intermediate_img = document.querySelector('#intermediateFlashImage');
+let intermediateFlashImages = [
+    'jesus.jpg',
+    'mans_ruin.jpg',
+    'pharoahs_horses.jpg',
+    'rose_of_no_mans_land.jpg'
+]
+
+let j = 0;
+
+function prev2() {
+    if(j <= 0) j = intermediateFlashImages.length;
+    j--;
+    return setImg2();
+}
+
+function next2() {
+    if(j >= intermediateFlashImages.length - 1) j = -1;
+    j++;
+    return setImg2();
+}
+
+function setImg2() {
+    return intermediate_img.setAttribute('src', 'tattoo_images/' + 'Intermediate/' + intermediateFlashImages[j])
+}
+
+//intermediate image slider function
+
+let badass_img = document.querySelector('#badassFlashImage');
+let badassFlashImages = [
+    'battle_royale.jpg',
+    'crucifiction.jpg',
+    'rock_of_ages.jpg'
+]
+
+let k = 0;
+
+function prev3() {
+    if(k <= 0) k = badassFlashImages.length;
+    k--;
+    return setImg3();
+}
+
+function next3() {
+    if(k >= badassFlashImages.length - 1) k = -1;
+    k++;
+    return setImg3();
+}
+
+function setImg3() {
+    return badass_img.setAttribute('src', 'tattoo_images/' + 'Badass/' + badassFlashImages[k])
 }
